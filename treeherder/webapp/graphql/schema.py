86a2366b7beaf1007e607eef0d503276b6e6188b
Cameron Dawson <cdawson@mozilla.com>
2017-06-06 15:39:30 -0700

    try one select_related

--- treeherder/webapp/graphql/schema.py
+++ treeherder/webapp/graphql/schema.py
@@ -141,7 +141,7 @@ class Query(graphene.ObjectType):
         return FailureClassification.objects.all()
 
     def resolve_all_pushes(self, args, context, info):
-        return Push.objects.filter(**args)
+        return Push.objects.filter(**args).select_related("jobs__job_details")
 
 
 schema = graphene.Schema(query=Query)
